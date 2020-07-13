23563bbd095a23e1d2051b4c05fa667396fcffc5
Montre Davis <montredavis@daviss-mbp.attlocal.net>
2020-07-12 21:25:10 -0500

    added trends and still working on the analyze algo

--- src/main/java/com/example/stockapi/models/DailyQuote.java
+++ src/main/java/com/example/stockapi/models/DailyQuote.java
@@ -4,16 +4,19 @@ package com.example.stockapi.models;
 import com.example.stockapi.entity.DailyAnalysis;
 import com.example.stockapi.utility.HoldRating;
 import com.example.stockapi.utility.Rating;
+import com.example.stockapi.utility.Trend;
 
 public class DailyQuote {
 
     public String ticker;
     public Rating rating;
     public String message;
+    public Trend trend;
 
     public DailyQuote(){
-        this.ticker = null;
+        this.ticker = "";
         this.rating = null;
-        this.message = null;
+        this.message = "";
+        this.trend = Trend.NUETRAL;
     }
 }
--- src/main/java/com/example/stockapi/tasks/AnalyzeTask.java
+++ src/main/java/com/example/stockapi/tasks/AnalyzeTask.java
@@ -6,8 +6,9 @@ import com.example.stockapi.dao.GlobalQuoteDao;
 import com.example.stockapi.dao.SymbolDao;
 import com.example.stockapi.entity.DailyAnalysis;
 import com.example.stockapi.entity.GlobalQuote;
+import com.example.stockapi.models.DailyQuote;
 import com.example.stockapi.models.subModels.MACD;
-import com.example.stockapi.utility.Indicators;
+import com.example.stockapi.utility.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
@@ -43,7 +44,7 @@ public class AnalyzeTask extends TimerTask {
     @Override
     public void run() {
         log.info("Running analyze task-- " + new Timestamp(System.currentTimeMillis()));
-        
+
         DailyAnalysis dailyAnalysis = new DailyAnalysis();
         try {
             dailyAnalysis = dailyAnalysisDao.grabBatchForProcessing();
@@ -54,92 +55,108 @@ public class AnalyzeTask extends TimerTask {
         if(dailyAnalysis != null && dailyAnalysis.dailyQuotes.size() > 0) {
 
             for (int i = 0; i < dailyAnalysis.dailyQuotes.size(); i++) {
-
+                DailyQuote dailyQuote = dailyAnalysis.dailyQuotes.get(i);
                 String symbol = dailyAnalysis.dailyQuotes.get(i).ticker;
 
-            GlobalQuote quote = new GlobalQuote();
-            // check to see if there is already an entry in the database for the quote today/
-            // if so then return that so we don't waste an external call
-            try {
-                quote = globalQuoteDao.getQuoteByTickerAndDateFromDb(symbol);
-                if(quote.id != null) {  }
-            } catch (Exception ex) {
-                log.error("There was an exception retrieving the information from the database: " + ex);
-            }
-
-            // if we don't have an entry grab the information externally, save and return this data.
-            try{
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                LocalDateTime now = LocalDateTime.now();
-
-                // retrieve stock quote information
-                quote = restTemplate.getForObject(String.format(ApplicationConstants.AV_GLOBAL_QUOTE, symbol), GlobalQuote.class);
-                quote.tickerSymbol = symbol;
-                quote.emaList = indicators.ExtractDailyEMA20(restTemplate, symbol);
-                quote.macdList = indicators.ExtractDailyMACD(restTemplate, symbol);
-                quote.recordedDate = dtf.format(now);
-
+                GlobalQuote quote = new GlobalQuote();
+                // check to see if there is already an entry in the database for the quote today/
+                // if so then return that so we don't waste an external call
                 try {
-                    // try to save to the mongo db
-                    globalQuoteDao.saveQuote(quote);
-
+                    quote = globalQuoteDao.getQuoteByTickerAndDateFromDb(symbol);
+                    if(quote.id != null) {  }
                 } catch (Exception ex) {
-                    log.error("There was an error saving to the database: " + ex);
+                    log.error("There was an exception retrieving the information from the database: " + ex);
                 }
 
-            } catch(Exception ex){
-                log.error("There was an exception grabbing the quote for ticker: " + symbol + " Execption: " + ex);
-            }
-            log.info("Completed grabbing Quote for ticket symbol: " + symbol);
+                // if we don't have an entry grab the information externally, save and return this data.
+                try{
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    LocalDateTime now = LocalDateTime.now();
 
-            // perform analysis
-            // we need a quote, ema and the macd values for the last 90 days
-            if(quote.globalQuote != null && quote.emaList.size() > 0 && quote.macdList.size() > 0) {
-                LocalDate today = LocalDate.parse(quote.recordedDate);
-                int dayInCycle = 0;
+                    // retrieve stock quote information
+                    quote = restTemplate.getForObject(String.format(ApplicationConstants.AV_GLOBAL_QUOTE, symbol), GlobalQuote.class);
+                    quote.tickerSymbol = symbol;
+                    quote.emaList = indicators.ExtractDailyEMA20(restTemplate, symbol);
+                    quote.macdList = indicators.ExtractDailyMACD(restTemplate, symbol);
+                    quote.recordedDate = dtf.format(now);
 
-                // analyze the MACD list first
-                // walk backwards and first search for the lowest and highest points.
-                // if you hit a value that is neither the highest or lowest point. Then we have a point of interest.
-                //
-                for (int m = 0; m < quote.macdList.size(); m++) {
-                    MACD current = quote.macdList.get(m);
-                    MACD previousDay = quote.macdList.get(m + 1);
+                    try {
+                        // try to save to the mongo db
+                        globalQuoteDao.saveQuote(quote);
 
-                    Double currentValue = Double.parseDouble(current.macdHist);
-                    Double previousValue = Double.parseDouble(previousDay.macdHist);
-
-                    // the current value is greater than zero than we are either in an upward trend, upward peak, or upward fall.
-                    // if we are in an upward trend we will see three (arbitrary) days of the previous values being lower or within decimals than the days before
-                    if (currentValue >= 0) {
-
-                        // possible upward trend
-                        if(currentValue >= previousValue) {
-
-                        }
-                        // possible downward trend
-                        else if (currentValue <= previousValue) {
-
-                        }
-
-                    } else {
-                        // if the current value is less than zero than we are either in a downward trend, downward peak, or downward fall.
-                        // possible upward trend
-                        if(currentValue >= previousValue) {
-
-                        }
-                        // possible downward trend
-                        else if (currentValue <= previousValue) {
-
-                        }
+                    } catch (Exception ex) {
+                        log.error("There was an error saving to the database: " + ex);
                     }
 
-                    // we need to break before we hit an index out of bounds exception
-                    if((m+1) == quote.macdList.size()){
-                        break;
+                } catch(Exception ex){
+                    log.error("There was an exception grabbing the quote for ticker: " + symbol + " Execption: " + ex);
+                }
+                log.info("Completed grabbing Quote for ticket symbol: " + symbol);
+
+                // perform analysis
+                // we need a quote, ema and the macd values for the last 90 days
+                if(quote.globalQuote != null && quote.emaList.size() > 0 && quote.macdList.size() > 0) {
+                    LocalDate today = LocalDate.parse(quote.recordedDate);
+                    int daysInCycle = 0;
+                    Rating rating;
+                    Trend currentTrend = Trend.NUETRAL;
+
+                    // analyze the MACD list first
+                    // walk backwards and first search for the lowest and highest points.
+                    // if you hit a value that is neither the highest or lowest point. Then we have a point of interest.
+                    //
+                    for (int m = 0; m < quote.macdList.size(); m++) {
+                        MACD current = quote.macdList.get(m);
+                        MACD previousDay = quote.macdList.get(m + 1);
+
+                        Double currentValue = Double.parseDouble(current.macdHist);
+                        Double previousValue = Double.parseDouble(previousDay.macdHist);
+
+                        // the current value is greater than zero than we are either in an upward trend, upward peak, or upward fall.
+                        // if we are in an upward trend we will see three (arbitrary) days of the previous values being lower or within decimals than the days before
+                        if (currentValue >= 0) {
+
+                            // possible upward trend
+                            if(currentValue >= previousValue) {
+                                daysInCycle++;
+                                currentTrend = Trend.POSITIVEUPWARD;
+
+                                if(daysInCycle >= 3) {
+                                    dailyQuote.trend = currentTrend;
+                                    dailyQuote.rating = new BuyRating(true);
+                                    break;
+                                }
+                            }
+                            // possible downward trend
+                            else if (currentValue <= previousValue) {
+                                daysInCycle++;
+                                currentTrend = Trend.POSITIVEDOWNWARD;
+
+                                if(daysInCycle >= 3) {
+                                    dailyQuote.trend = currentTrend;
+                                    dailyQuote.rating = new SellRating(true);
+                                    break;
+                                }
+                            }
+
+                        } else {
+                            // if the current value is less than zero than we are either in a downward trend, downward peak, or downward fall.
+                            // possible upward trend
+                            if(currentValue >= previousValue) {
+
+                            }
+                            // possible downward trend
+                            else if (currentValue <= previousValue) {
+
+                            }
+                        }
+
+                        // we need to break before we hit an index out of bounds exception
+                        if((m+1) == quote.macdList.size()){
+                            break;
+                        }
                     }
                 }
-            }
             }
         }
         log.info("Finished analyze task-- " + new Timestamp(System.currentTimeMillis()));
